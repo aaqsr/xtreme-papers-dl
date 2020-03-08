b23d21157f6c5067e74b0ed5096e409c1d588543
Awab Qureshi <awab.qureshi@gmail.com>
2020-03-08 15:47:43 +0500

    Changed to the built in html parser

--- XTRME.py
+++ XTRME.py
@@ -9,18 +9,20 @@ import os
 ''' define global variables'''
 
 url_list = {
-
-	'9231':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Mathematics - Further (9231)/',
-	'9608':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Computer Science (9608)/',
-	'9691':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Computing (9691)/',
-	'9698':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Psychology (9698)/',
-	'9700':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Biology (9700)/',
-	'9701':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Chemistry (9701)/',
-	'9702':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Physics (9702)/',
-	'9706':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Accounting (9706)/',
-	'9707':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Business Studies (9707)/',
-	'9708':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Economics (9708)/',
-	'9709':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Mathematics (9709)/'
+    
+    '5070':'https://papers.xtremepape.rs/CAIE/O%20Level/Chemistry%20(5070)/'
+	
+ 	# '9231':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Mathematics - Further (9231)/',
+	# '9608':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Computer Science (9608)/',
+	# '9691':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Computing (9691)/',
+	# '9698':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Psychology (9698)/',
+	# '9700':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Biology (9700)/',
+	# '9701':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Chemistry (9701)/',
+	# '9702':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Physics (9702)/',
+	# '9706':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Accounting (9706)/',
+	# '9707':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Business Studies (9707)/',
+	# '9708':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Economics (9708)/',
+	# '9709':'http://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/Mathematics (9709)/'
 }
 online_url = 'https://papers.xtremepapers.com/CIE/Cambridge International A and AS Level/'
 
@@ -70,7 +72,7 @@ def get_code():
 	''' get the syllabus code for every subject on xtremepaper'''
 
 	html = requests.get(online_url)
-	soup = BeautifulSoup(html.text,'lxml')
+	soup = BeautifulSoup(html.text,'html.parser')
 	datas = soup.select('td.autoindex_td > a ')
 	if datas != []:
 		# get rid of parent dir
@@ -92,22 +94,20 @@ def get_code():
 		print('NetWork error')
 		return -1
 
-
-
 def get_papers(url):
 
 	'''download all the files at the url provided'''
 	new_hrefs = []
 	# obatin url
 	html = requests.get(url)
-	soup = BeautifulSoup(html.text,'lxml')
+	soup = BeautifulSoup(html.text,'html.parser')
 	hrefs = soup.select('td.autoindex_td > a')
 
 	if hrefs == []:
 		print('wrong url provided')
 		return -1
 	hrefs.pop(0)
-	print('Now starts to download, this may (defintely) take some times so you may want to go and grab yourself some drink and let this programe run alone.')
+	print('Now starts to download, this may (definitely) take some time so you may want to let this program run alone.')
 	
 	#create folder
 	folder_path = '.\\'+url.split('/')[-2]+'\\'
@@ -133,55 +133,3 @@ def get_papers(url):
 
 	print('successfully downloaded all the files available at {}'.format(url))
 	return 0
-
-
-
-
-
-
-
-def main():
-
-	''' handles input and outputs '''
-
-	# get code
-	print('please provide the syllabus number of the papers you wants to download:', end ='')
-	url_key = input()
-
-	# local code
-	if url_key in url_list:
-
-		print('detected syllabus code, processing...')
-		get_papers(url_list[url_key])
-
-	# online code
-	elif url_key.isdigit():
-		print('syllabus code did not match local database, do you wish to get online database for syllabus code?')
-
-		ans = input()
-		if ans == 'y' or ans == 'yes':
-			# seraching online
-			print('Searching at online data base at {}'.format(online_url))
-			get_code()
-			if url_key in url_online:
-				print('detected syllabus code, processing...')
-				get_papers(url_online[url_key])
-			else:
-				print('invalid syllabus code.')
-		else:
-			print('invalid syllabus code.')
-	# provide url
-	elif url_key.startswith('http://') or url_key.startswith('https://'):
-
-		print('url detected')
-		print('downloading form the website {}'.format(url_key))
-		get_papers(url_key)
-
-	else:
-		print('invalid input please check your syllabus code / start your url with http')
-
-
-
-
-if __name__ == '__main__':
-	main()
